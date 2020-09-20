64177854b8d9719f3d3855abec17e1123e1334df
Flamethrow <r2flamethrow@gmail.com>
2020-09-20 16:29:38 -0300

    Alternancia de cores e diminuicao do tempo de troca

--- principal.js
+++ principal.js
@@ -11,8 +11,9 @@ document
 
 function trocaBanner() {
   bannerAtual = bannerAtual == 0 ? 1 : 0;
+  h2.style.color = h2.style.color == "yellow" ? "red" : "yellow";
 
   document.querySelector("h2#mensagem").innerHTML = banners[bannerAtual];
 }
 
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
